bc23d598d5b38532716a11445561c6b4a5e7b527
Dong Leticia <dongleticia@gmail.com>
2022-09-01 14:35:37 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,9 @@
+const result =[{ name:"comfort Yelfaare", class: "form three", grade:"8"},
+               { name:"Atif Adamu", class: "form three", grade:"30"},
+                { name:"comfort Yelfaare", class: "form three", grade:"52"},
+                 { name:"comfort Yelfaare", class: "form three", grade:"9"},
+                  { name:"comfort Yelfaare", class: "form three", grade:"8"},
+                 
+
+]
+module.exports=result
